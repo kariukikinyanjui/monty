5672b2dde1fc96f28910d393f8f1cfa7821c1d72
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-08-24 19:50:37 +0000

    Executes the pint opcode

--- exec.c
+++ exec.c
@@ -12,6 +12,7 @@ int _exec(char *content, stack_t **stack, unsigned int line_no, FILE *file)
 {
 	instruction_t opst[] = {
 		{"push", _push}, {"pall", _pall},
+		{"pint", _pint},
 		{"queue", _queue},
 		{NULL, NULL}
 	};
