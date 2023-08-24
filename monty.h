0c08dee3be9c922df8411d671b3a7412230d425e
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-08-24 19:48:51 +0000

    Updated the header file

--- monty.h
+++ monty.h
@@ -58,4 +58,5 @@ void _addqueue(stack_t **head, int n);
 void _queue(stack_t **head, unsigned int line_no);
 int _exec(char *content, stack_t **stack, unsigned int line_no, FILE *file);
 ssize_t getline(char **lineptr, size_t *n, FILE *stream);
+void _pint(stack_t **head, unsigned int line_no);
 #endif
