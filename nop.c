2f35a8f99f2ca61ab9f79b6bcd9894083e4ab6b8
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-08-25 00:36:46 +0000

    Opcode nop doesn't do anything

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * _nop - doesn't do anything
+ * @head: pointer to the head of the stack/queue
+ * @line_no: line number
+ */
+void _nop(stack_t **head, unsigned int line_no)
+{
+	(void)head;
+	(void)line_no;
+}
